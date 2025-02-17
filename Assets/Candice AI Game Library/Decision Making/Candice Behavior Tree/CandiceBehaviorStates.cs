986218a85662a8239baffa2dd0da396602bd0d2e
Mike Natuyamba <54777896+Natubeast@users.noreply.github.com>
2025-02-17 22:38:02 +0200

    Added Decision Making: Behavior Trees

---
+++ Assets/Candice AI Game Library/Decision Making/Candice Behavior Tree/CandiceBehaviorStates.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CandiceAI.Games.DecisionMaking
+{
+    public enum CandiceBehaviorStates
+    {
+        SUCCESS,
+        FAILURE,
+        RUNNING,
+    }
+}
